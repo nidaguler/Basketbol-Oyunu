7ffa53e500e67be37e88bcb13f7e9d96ceb88947
NG <46129872+nidgul@users.noreply.github.com>
2019-05-15 02:35:54 +0300

    Add files via upload

---
+++ Scripts/ScoreArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreArea : MonoBehaviour
+{
+
+    public GameObject effectObject;
+   
+
+    public object Text { get; private set; }
+
+    void Start()
+    {
+        
+        effectObject.SetActive(false);
+    
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.GetComponent<Ball>() != null)
+        {
+            effectObject.SetActive(true);
+           
+            
+            
+            
+        }
+    }
+}
